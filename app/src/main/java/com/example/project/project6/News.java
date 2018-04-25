629d178de59436cde642e2faf2d70c4fbc98a125
atheer <atheerabdullaha@outlook.com>
2018-04-25 14:33:58 +0300

    initial commit

---
+++ app/src/main/java/com/example/project/project6/News.java
@@ -0,0 +1,55 @@
+package com.example.project.project6;
+
+public class News {
+
+
+    private String mTitle;
+    private String mSection;
+    private String mAuthor;
+    private String mDate;
+    private String mUrl;
+
+
+    public News(String title,  String url ) {
+        mTitle= title;
+        mUrl = url;
+    }
+    public News(String title,  String url, String section,String date ) {
+        mTitle= title;
+        mUrl = url;
+        mSection=section;
+        mDate=date;
+    }
+        public News(String title,  String section, String author,String date, String url ) {
+            mTitle= title;
+            mSection = section;
+            mAuthor=author;
+            mDate=date;
+            mUrl = url;
+        }
+
+    public String getmAuthor() {
+        return mAuthor;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public String getmSection() {
+        return mSection;
+    }
+
+    public String getmDate() {
+        return mDate;
+    }
+
+    public String getmUrl() {
+        return mUrl;
+    }
+
+    public String getUrl() {
+            return mUrl;
+        }
+
+}
